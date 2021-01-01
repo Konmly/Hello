e4b3566e840559a7069e0f5f0d0572deca360e06
Kristina <kristino4ka_17@mail.ru>
2021-01-01 17:04:16 +0300

    Ne rabotaet yslovie a ostal'noe rabotaet

--- projecty/pip1.rb
+++ projecty/pip1.rb
@@ -9,18 +9,18 @@ def result=(new_value)
 end
 def name_user(name)
   puts "Your name?"
-  @name=gets.chomp
+  @name=gets.chomp.to_i
   puts "Your name: #{@name}"
 end
 def hight_user(hight)
   puts "Your hight?"
-  @hight=gets.chomp
-  puts "Your hight: #{@hight}"
-  if @hight == [/^\d+$/]
-    @hight=gets.chomp
-  elsif @hight !=[/^\d+$/]
-    raise "Eror"
-  end
+  @hight=gets.chomp.to_i
+  if @hight != [/^\d+$/]
+    return "Eror"
+  elsif @hight ==[/^\d+$/]
+    puts "Your hight: #{@hight}"
+    end
+end
   def result_user(result)
     @result=(@hight-110)*1.15
     puts "Result: #{@result}"
@@ -30,7 +30,7 @@ def hight_user(hight)
     puts "Your weight is not optimal!"
     end
   end
-end
+
 name_user(@name)
 hight_user(@hight)
 result_user(@result)

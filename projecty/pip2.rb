347c322c0e5418f8466f3b99494773a4ab620a81
Kristina <kristino4ka_17@mail.ru>
2021-01-02 19:04:17 +0300

    HHH

---
+++ projecty/pip2.rb
@@ -0,0 +1,29 @@
+def base_user
+  puts "Enter base?"
+  base=gets.chomp
+  if base =~/^\d+$/
+    puts "Base: #{base}"
+    return base.to_i
+  else
+    puts "Error"
+  end
+  return base
+end
+def height_user
+  puts "Enter height?"
+  height=gets.chomp
+  if height =~/^\d+$/
+    puts "Height: #{height}"
+    return height.to_i
+  else
+    puts "Error"
+  end
+  return height
+end
+def rezult_s(base,height)
+  s=1/(2*base*height)
+  puts "S= #{s.to_f}"
+end
+base= base_user
+height=height_user
+rezult_s(base,height)
